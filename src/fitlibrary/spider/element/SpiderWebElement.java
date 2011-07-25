20288e26202c45bd606249230a0174a0b5dcd703
Darren Rowley <Darren Rowley@DESKTOP.(none)>
2011-07-25 20:27:49 +1200

    improved yet again Firefox handling with options()

--- src/fitlibrary/spider/element/SpiderWebElement.java
+++ src/fitlibrary/spider/element/SpiderWebElement.java
@@ -1,11 +1,13 @@
 package fitlibrary.spider.element;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxWebElement;
 
 import util.StringUtil;
 
@@ -28,14 +30,13 @@ public class SpiderWebElement implements WebElement {
 
         // different behaviour in FireFox than other browsers, FireFox returns the text when value attribute missing other browsers return blank 
         // make FireFox conform to other browsers
-        if (!StringUtil.isBlank(value) && value.equals(getText())) {
-              if (!rawHtmlofParent.matches("<option.*value=[',\"]"+value+"[',\"].*>"+value+"</option>")) {
-                    return "";
-              }
+        if (!StringUtil.isBlank(value) && value.equals(getText()) && (wrappedElement instanceof FirefoxWebElement)) {
+            if (!Pattern.compile(".*<option.*?value=[',\"]"+value+"[',\"].*?>"+value+"</option>.*",Pattern.DOTALL).matcher(rawHtmlofParent).matches()) {
+                return "";
+          }
         }
 
         return value;
-
   }
 	
 	// --- wrapped implementations of WebElement
@@ -44,72 +45,57 @@ public class SpiderWebElement implements WebElement {
 	public void click() {
 		wrappedElement.click();
 	}
-
 	@Override
 	public void submit() {
 		wrappedElement.submit();
 	}
-
 	public void sendKeys(CharSequence... keysToSend) {
 		wrappedElement.sendKeys(keysToSend);
 	}
-
 	@Override
 	public void clear() {
 		wrappedElement.clear();
 	}
-
 	@Override
 	public String getTagName() {
 		return wrappedElement.getTagName();
 	}
-
 	@Override
 	public String getAttribute(String name) {
 		return wrappedElement.getAttribute(name);
 	}
-
 	@Override
 	public boolean isSelected() {
-		// TODO Auto-generated method stub
 		return wrappedElement.isSelected();
 	}
-
 	@Override
 	public boolean isEnabled() {
 		return wrappedElement.isEnabled();
 	}
-
 	@Override
 	public String getText() {
 		return wrappedElement.getText();
 	}
-
 	@Override
 	public List<WebElement> findElements(By by) {
 		return wrappedElement.findElements(by);
 	}
-
 	@Override
 	public WebElement findElement(By by) {
 		return wrappedElement.findElement(by);
 	}
-
 	@Override
 	public boolean isDisplayed() {
 		return wrappedElement.isDisplayed();
 	}
-
 	@Override
 	public Point getLocation() {
 		return wrappedElement.getLocation();
 	}
-
 	@Override
 	public Dimension getSize() {
 		return wrappedElement.getSize();
 	}
-
 	@Override
 	public String getCssValue(String propertyName) {
 		return wrappedElement.getCssValue(propertyName);
